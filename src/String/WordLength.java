d0f369a8996ea2933de7b012f73424d78c00f86c
kushal4345 <jeeultimate4345@gmail.com>
2025-06-03 00:52:59 +0530

    solve word length problem

---
+++ src/String/WordLength.java
@@ -0,0 +1,25 @@
+package String;
+
+public class WordLength {
+    public static void main(String args[]){
+//        String s = "   fly me   to   the moon  ";
+////        String s =  "Hello World";
+//        int length = 0;
+//        int i = s.length() - 1;
+//
+//        // Step 1: Skip trailing spaces
+//        while (i >= 0 && s.charAt(i) == ' ') {
+//            i--;
+//        }
+//
+//        // Step 2: Count characters in the last word
+//        while (i >= 0 && s.charAt(i) != ' ') {
+//            length++;
+//            i--;
+//        }
+
+        System.out.println(968*123);
+    }
+
+    }
+
