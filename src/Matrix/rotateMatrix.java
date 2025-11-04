c6d83a985a78f5bf93cae1c70f95bef2fcd58fd4
kushal4345 <155578739+kushal4345@users.noreply.github.com>
2025-11-04 23:12:08 +0530

    rotate image

---
+++ src/Matrix/rotateMatrix.java
@@ -0,0 +1,25 @@
+package Matrix;
+
+public class rotateMatrix {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = rotated[i][j];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate( arr);
+    }
+}
