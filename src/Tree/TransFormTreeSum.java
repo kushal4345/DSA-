a589628d6344fd7ac40cd70c7ded87e93eedee64
kushal4345 <155578739+kushal4345@users.noreply.github.com>
2025-11-11 11:18:28 +0530

    transform tree in sum

---
+++ src/Tree/TransFormTreeSum.java
@@ -0,0 +1,36 @@
+package Tree;
+
+public  class TransFormTreeSum {
+    public static void toSumTree(Node root) {
+        convert(root);
+    }
+
+   public static  int convert(Node root) {
+        if (root == null) return 0;
+
+        int oldValue = root.val;
+
+        int leftSum = convert(root.left);
+        int rightSum = convert(root.right);
+
+        root.val = leftSum + rightSum;
+
+        return root.val + oldValue;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(9);
+        Node d = new Node(2);
+        Node e = new Node(7);
+        Node f = new Node(6);
+        Node g = new Node(5);
+
+        root.left = b;
+        root.right = c;
+        c.left = d;
+        d.right = e;
+        b.left = f;
+        toSumTree(root);
+    }
+}
