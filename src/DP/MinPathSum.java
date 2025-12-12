c60efdfd11b30e4757da823256de68662ca4c8a6
kushal4345 <155578739+kushal4345@users.noreply.github.com>
2025-12-12 18:46:36 +0530

    min path sum memoization

---
+++ src/DP/MinPathSum.java
@@ -0,0 +1,32 @@
+package DP;
+
+import java.util.Arrays;
+
+public class MinPathSum {
+   static  int dfs(int i, int j, int[][] grid,int dp[][]) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+
+        if (i >= n || j >= m) return Integer.MAX_VALUE;
+
+        if (i == n - 1 && j == m - 1)
+            return grid[i][j];
+
+        if(dp[i][j]!=-1)return dp[i][j];
+        int right = dfs(i, j + 1, grid,dp);
+        int down = dfs(i + 1, j, grid,dp);
+
+        return dp[i][j] = grid[i][j] + Math.min(right, down);
+    }
+
+    public static void main(String[] args) {
+        int [][]grid = {{1,3,1},{1,5,1},{4,2,1}};
+        int dp[][] =  new int[grid.length][grid[0].length];
+        for(int arr[] : dp){
+            Arrays.fill(arr,-1);
+        }
+        System.out.println(dfs(0,0,grid,dp));
+    }
+
+}
